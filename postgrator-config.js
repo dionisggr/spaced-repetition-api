0dc513ce4acf5a8dfbd2248e0de6b71c1e0be78e
Dio Gonzalez <dionisggr@gmail.com>
2021-02-05 12:17:27 -0700

    postgrator changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,11 +5,10 @@ pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 const database = (process.env.NODE_ENV === 'test')
   ? `${process.env.MIGRATION_DB_NAME}-test`
-  : process.env.MIGRATION_DB_NAME
+  : process.env.MIGRATION_DB_NAME;
 
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DATABASE_URL
 }
-
